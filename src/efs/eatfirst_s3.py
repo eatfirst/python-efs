b5e6f2be2838b54efac74b360ee8893893deef29
Jayson Reis <santosdosreis@gmail.com>
2018-08-23 17:18:33 +0200

    Remove trailing comma
    Signed-off-by: Jayson Reis <santosdosreis@gmail.com>

--- src/efs/eatfirst_s3.py
+++ src/efs/eatfirst_s3.py
@@ -7,4 +7,4 @@ from fs_s3fs import S3FS
 class EatFirstS3(S3FS):
     """Extension of S3FS class, fixing it to work with python 3k."""
 
-    __repr__ = autorepr(['bucket', 'aws_access_key_id',], bucket=lambda self: self._bucket_name)
+    __repr__ = autorepr(['bucket', 'aws_access_key_id'], bucket=lambda self: self._bucket_name)
